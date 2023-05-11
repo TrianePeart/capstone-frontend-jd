5750ca11147ac2ec38f4688859eb505517e1d100
TrianePeart <TrianePeart@gmail.com>
2023-05-11 17:48:00 -0400

    minor error corrected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,7 +56,7 @@ function NavBar() {
             </div>
           )}
         </div>
-        <div
+        <a href='settings'
           className='settings'
           onClick={handleSettingsClick}
           onKeyDown={e => {
@@ -69,7 +69,7 @@ function NavBar() {
           //This role is creating this error
         >
           <button className='settings-btn'>Settings</button>
-        </div>
+        </a>
       </nav>
       {showSettings && (
         <div className='settings-sidebar'>
